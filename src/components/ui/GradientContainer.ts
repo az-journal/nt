b5f27e6a3e85c3e098754dca52c802039735ec8c
slvDev <slava.serbov@gmail.com>
2022-02-21 10:35:52 +0200

    UI fixes

--- src/components/ui/GradientContainer.ts
+++ src/components/ui/GradientContainer.ts
@@ -2,6 +2,9 @@ import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 
 const GradientContainer = styled(Container)({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
     background: 'radial-gradient(circle at 50% 0%, #148CAA, #001473)',
 });
 
--- src/views/Explore.tsx
+++ src/views/Explore.tsx
@@ -7,6 +7,7 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import GradientContainer from '../components/ui/GradientContainer';
+import Header from '../components/Header';
 
 const Explore = () => {
     const [value, setValue] = useState('1');
@@ -17,24 +18,23 @@ const Explore = () => {
 
     return (
         <GradientContainer maxWidth={false}>
-            <Container maxWidth="lg">
-                <TabContext value={value}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <TabList onChange={handleChange} aria-label="lab API tabs example">
-                            <Tab label="Nafta Pool" value="1" />
-                            <Tab label="My" value="2" />
-                            <Tab label="Lending" value="3" />
-                            <Tab label="Borrowed" value="4" />
-                        </TabList>
-                    </Box>
-                    <TabPanel value="1">
-                        <NaftaPool />
-                    </TabPanel>
-                    <TabPanel value="2">Item Two</TabPanel>
-                    <TabPanel value="3">Item Three</TabPanel>
-                    <TabPanel value="4">Item Four</TabPanel>
-                </TabContext>
-            </Container>
+            <Header />
+            <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <TabList onChange={handleChange} aria-label="lab API tabs example">
+                        <Tab label="Nafta Pool" value="1" />
+                        <Tab label="My" value="2" />
+                        <Tab label="Lending" value="3" />
+                        <Tab label="Borrowed" value="4" />
+                    </TabList>
+                </Box>
+                <TabPanel value="1">
+                    <NaftaPool />
+                </TabPanel>
+                <TabPanel value="2">Item Two</TabPanel>
+                <TabPanel value="3">Item Three</TabPanel>
+                <TabPanel value="4">Item Four</TabPanel>
+            </TabContext>
         </GradientContainer>
     );
 };
