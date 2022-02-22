011ba828752be3c04ddb8a63a946d432254969c7
slvDev <slava.serbov@gmail.com>
2022-02-22 16:13:38 +0200

    fetch Earnings

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,27 +1,38 @@
 import { useEffect } from 'react';
-import { useMoralis } from 'react-moralis';
+import { useMoralis, useWeb3ExecuteFunction } from 'react-moralis';
 import { styled } from '@mui/material/styles';
 import { Typography } from '@mui/material';
+import { formatAddr } from '../utils/formatAddr';
+import { getNaftaAddr } from '../utils/getAddresses';
+import { getFormattedWei } from '../utils/weiConverter';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import LogoutIcon from '@mui/icons-material/Logout';
-import { formatAddr } from '../utils/formatAddr';
+import NaftaAbi from '../abis/Nafta.json';
 
-const StyledButton = styled(Button)({
+const LoginButton = styled(Button)({
     width: '150px',
-    height: '40px',
-    borderRadius: '20px',
+    height: '44px',
+    borderRadius: '22px',
     boxShadow: '0px 5px RGBA(0, 0, 0, 0.25)',
 });
+
+const WithdrawButton = styled(Button)(({ theme }) => ({
+    width: '130px',
+    height: '24px',
+    borderRadius: '13px',
+    border: `1px solid ${theme.palette.text.primary}`,
+}));
 
 const User = styled(Box)(({ theme }) => ({
     display: 'flex',
     justifyContent: 'flex-end',
     alignItems: 'center',
-    height: '40px',
+    height: '44px',
+    borderRadius: '22px',
     background: theme.palette.primary.main,
-    borderRadius: '20px',
+    boxShadow: '0px 5px RGBA(0, 0, 0, 0.25)',
 }));
 
 const Wallet = () => {
@@ -33,22 +44,58 @@ const Wallet = () => {
         logout,
         isAuthenticated,
         isAuthenticating,
+        web3,
         enableWeb3,
         isWeb3Enabled,
         account,
         chainId,
     } = useMoralis();
+    const { fetch } = useWeb3ExecuteFunction();
 
     useEffect(() => {
         if (isAuthenticated) enableWeb3();
     }, [isAuthenticated]);
 
+    useEffect(() => {
+        const getEarnings = async () => {
+            const options = {
+                abi: NaftaAbi.abi,
+                contractAddress: getNaftaAddr(chainId),
+                functionName: 'earnings',
+                params: { '': account },
+            };
+            const earnings = Number(await Moralis.executeFunction(options));
+            console.log(earnings);
+        };
+        if (isWeb3Enabled) getEarnings();
+    }, [isWeb3Enabled, chainId, account]);
+
+    const handleWithdraw = () => {
+        const options = {
+            abi: NaftaAbi.abi,
+            contractAddress: getNaftaAddr(chainId),
+            functionName: 'withdrawEarnings',
+        };
+        if (isWeb3Enabled) fetch({ params: options });
+    };
+
+    // useEffect(() => {
+    //     console.log(Number(earnings));
+    //     if (typeof earnings === 'string') console.log('earnings', earnings);
+    // }, [earnings]);
+
     return (
         <>
             {isAuthenticated && (
                 <Box display="flex" alignItems="center">
+                    <Box textAlign="center" mr={2}>
+                        <Typography variant="body2">Earned: {getFormattedWei(Number(10))}</Typography>
+                        <WithdrawButton onClick={handleWithdraw}>
+                            <Typography variant="body2">Withdraw</Typography>
+                        </WithdrawButton>
+                    </Box>
                     <User>
-                        <Typography mx="10px" variant="body2">
+                        <Typography mx={2} variant="body2">
                             {formatAddr(account)}
                         </Typography>
                         <Box
@@ -67,7 +114,7 @@ const Wallet = () => {
                 </Box>
             )}
             {!isAuthenticated && (
-                <StyledButton
+                <LoginButton
                     variant="contained"
                     disabled={isAuthenticating}
                     onClick={() =>
@@ -81,7 +128,7 @@ const Wallet = () => {
                     <Typography sx={{ color: '#000' }} variant="body1">
                         {isMetamask ? 'Login' : 'Install MetaMask'}
                     </Typography>
-                </StyledButton>
+                </LoginButton>
             )}
         </>
     );
